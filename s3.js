fa3cae34cd938cce75cb9db9b437b5e88a6afbd6
Liad Yosef <liad@duda.co>
2020-01-09 12:39:33 +0200

    change structure

---
+++ s3.js
@@ -0,0 +1,23 @@
+const aws = require("aws-sdk");
+const ZipModifier = require('./utils/zipModifier');
+const s3 = new aws.S3();
+
+module.exports = class S3ZipModifier {
+    async loadFromS3(event = {}) {
+        this.event = event;
+        const { bucket, key } = event;
+        const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+        this.file = data.Body;
+        this.zipModifier = new ZipModifier();
+        await this.zipModifier.loadZip(this.file);
+        return this.zipModifier;
+    }
+
+    async saveToS3(options = {}) {
+        const modifiedZip = await this.zipModifier.exportZip();
+        const destBucket = options.bucket || this.event.destBucket || this.event.bucket;
+        const destKey = options.key || this.event.destKey || `${this.event.key}_modified`;
+        await s3.putObject({ Bucket: destBucket, Key: destKey, Body: modifiedZip }).promise();
+        return { bucket: destBucket, key: destKey };
+    }
+}
