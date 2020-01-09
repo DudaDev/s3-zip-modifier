0b479b83ddd7cdf994758dcbeebbb2baf290cf5c
Liad Yosef <liad@duda.co>
2020-01-09 13:54:40 +0200

    minor

--- s3.js
+++ s3.js
@@ -3,9 +3,9 @@ const ZipModifier = require('./utils/zipModifier');
 const s3 = new aws.S3();
 
 module.exports = class S3ZipModifier {
-    async loadFromS3(event = {}) {
-        this.event = event;
-        const { bucket, key } = event;
+    async loadFromS3(s3LocationParams = {}) {
+        this.s3LocationParams = s3LocationParams;
+        const { bucket, key } = s3LocationParams;
         const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
         this.file = data.Body;
         this.zipModifier = new ZipModifier();
@@ -15,8 +15,8 @@ module.exports = class S3ZipModifier {
 
     async saveToS3(options = {}) {
         const modifiedZip = await this.zipModifier.exportZip();
-        const destBucket = options.bucket || this.event.destBucket || this.event.bucket;
-        const destKey = options.key || this.event.destKey || `${this.event.key}_modified`;
+        const destBucket = options.bucket || this.s3LocationParams.destBucket || this.s3LocationParams.bucket;
+        const destKey = options.key || this.s3LocationParams.destKey || `${this.s3LocationParams.key}_modified`;
         await s3.putObject({ Bucket: destBucket, Key: destKey, Body: modifiedZip }).promise();
         return { bucket: destBucket, key: destKey };
     }
