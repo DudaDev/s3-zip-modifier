71fe7c7171c16baec6d878a979053be1f703361d
Liad Yosef <liad@duda.co>
2020-01-09 17:31:51 +0200

    add lcoal file interface, change functions

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-exports.s3 = require('./s3.js');
-exports.ZipModifier = require('./utils/zipModifier.js');
+exports.s3 = require("./s3.js");
+exports.file = require("./file.js");
+exports.ZipModifier = require("./utils/zipModifier.js");
--- s3.js
+++ s3.js
@@ -1,23 +1,35 @@
 const aws = require("aws-sdk");
-const ZipModifier = require('./utils/zipModifier');
+const ZipModifier = require("./utils/zipModifier");
 const s3 = new aws.S3();
 
 module.exports = class S3ZipModifier {
-    async loadFromS3(s3LocationParams = {}) {
-        this.s3LocationParams = s3LocationParams;
-        const { bucket, key } = s3LocationParams;
-        const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
-        this.file = data.Body;
-        this.zipModifier = new ZipModifier();
-        await this.zipModifier.loadZip(this.file);
-        return this.zipModifier;
-    }
+  constructor({ verbose } = {}) {
+    this.verbose = verbose;
+  }
 
-    async saveToS3(options = {}) {
-        const modifiedZip = await this.zipModifier.exportZip();
-        const destBucket = options.bucket || this.s3LocationParams.destBucket || this.s3LocationParams.bucket;
-        const destKey = options.key || this.s3LocationParams.destKey || `${this.s3LocationParams.key}_modified`;
-        await s3.putObject({ Bucket: destBucket, Key: destKey, Body: modifiedZip }).promise();
-        return { bucket: destBucket, key: destKey };
-    }
-}
+  async loadZip(s3LocationParams = {}) {
+    this.s3LocationParams = s3LocationParams;
+    const { bucket, key } = s3LocationParams;
+    const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+    this.file = data.Body;
+    this.zipModifier = new ZipModifier({ verbose: this.verbose });
+    await this.zipModifier.loadZip(this.file);
+    return this.zipModifier;
+  }
+
+  async saveZip(options = {}) {
+    const modifiedZip = await this.zipModifier.exportZip();
+    const destBucket =
+      options.bucket ||
+      this.s3LocationParams.destBucket ||
+      this.s3LocationParams.bucket;
+    const destKey =
+      options.key ||
+      this.s3LocationParams.destKey ||
+      `${this.s3LocationParams.key}_modified`;
+    await s3
+      .putObject({ Bucket: destBucket, Key: destKey, Body: modifiedZip })
+      .promise();
+    return { bucket: destBucket, key: destKey };
+  }
+};
